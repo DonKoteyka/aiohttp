9f90322091420f146405f35ba733cf6a7a3772e6
DonKoteyka <777zik@gmail.com>
2024-01-17 19:47:05 +0300

    24.01.17

--- client.py
+++ client.py
@@ -2,10 +2,19 @@ import asyncio
 import aiohttp
 
 
+data_json = {
+    'article': 'valideted art',
+    'description': 'some discription',
+    'owner': 'Host'       
+             }
+
 async def main():
     client = aiohttp.ClientSession()
-    response = await client.post('http://127.0.0.1:8080')
+    response = await client.post('http://0.0.0.0:8080/api/', json=data_json)
     print(response.status)
+    # print(await response.json())
+
+    await client.close()
 
 
 
--- server.py
+++ server.py
@@ -1,7 +1,22 @@
 from aiohttp import web
 
 
-app = web.Application()
+app = web.Application(debug=True)
 
+async def hello_world(request):
+    return web.json_response({'hello': 'world'})
+
+async def article_view(request):
+    json_data = await request.json()
+    print(json_data)
+
+
+
+
+app.add_routes((
+    web.get('/', hello_world),
+    web.post('/api', article_view),
+    # web.get('/api/<int:article_id>', article_view),
+    ))
 
 web.run_app(app)
