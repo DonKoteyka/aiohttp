77e11d6fef23cf2e308ca8f57ec237b83e046dac
DonKoteyka <777zik@gmail.com>
2024-01-20 16:33:43 +0300

    24.01.20 #1

--- client.py
+++ client.py
@@ -1,27 +1,32 @@
 import asyncio
+
 import aiohttp
 
-numb = 4
+numb = " create_val_2"
 
 data_json = {
-    'article': f'valideted art{numb}',
-    'description': f'some discription{numb}',
-    'owner': f'Host{numb}'       
-             }
+    "article": f"valideted art{numb}",
+    "description": f"some discription{numb}",
+    "owner": f"Host{numb}",
+}
+
 
 async def main():
     client = aiohttp.ClientSession()
-    response = await client.post('http://0.0.0.0:8080/api', json=data_json)
+    response = await client.post("http://0.0.0.0:8080/api", json=data_json)
     print(response.status)
-    # response_2 = await client.get('http://0.0.0.0:8080/')
-    # print(response_2)
-    # print(await response.json())
+    # response_2 = await client.get('http://0.0.0.0:8080/api/2')
+    # print(response_2.status)
+    # print(await response_2.json())
+    # response = await client.delete('http://0.0.0.0:8080/api/10')
+    # print(response.status)
+
+    # response = await client.patch('http://0.0.0.0:8080/api/12', json=data_json)
+    # print(response.status)
+    print(await response.json())
 
     await client.close()
 
 
-
-
-if __name__ == '__main__':
+if __name__ == "__main__":
     asyncio.run(main())
-
